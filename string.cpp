65cdfe9ea9c3e35b3077751b5dc119939169f6f9
tejasjadhav06 <jadhavtejas405@gmail.com>
2024-09-25 16:20:16 +0530

    Concate String using Concate opeartor and append method.

--- string.cpp
+++ string.cpp
@@ -4,5 +4,14 @@ int main(){
     string name = "Tejas";
     string last = " Jadhav";
     cout << name << last;
+
+    // Concatenation 
+    cout << "\nConcatenation using + Operator: \n";
+    cout << name + last << "\n";
+
+    // Append
+    cout << "Concate String using append method: ";
+    cout << name.append(" Jadhav");
+
     return 0;
 }
