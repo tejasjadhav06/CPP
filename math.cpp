790a960d779bea2de74ed6195029ae59a9740762
tejasjadhav06 <jadhavtejas405@gmail.com>
2024-09-25 16:33:14 +0530

    Square Root Function

--- math.cpp
+++ math.cpp
@@ -4,5 +4,6 @@ using namespace std;
 int main(){
     cout << "Maximum number from two number is : " << max(5,85);
     cout << "\nMinimum number from two number is : " << max(5,85);
+    cout << "\n Square Root Function : " << sqrt(64);
     return 0;
 }
