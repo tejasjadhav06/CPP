cb72d9538c28c0d7a56a81aa91874945cea316ad
tejasjadhav06 <jadhavtejas405@gmail.com>
2024-09-25 16:34:53 +0530

    Round Number Function

--- math.cpp
+++ math.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int main(){
     cout << "Maximum number from two number is : " << max(5,85);
     cout << "\nMinimum number from two number is : " << max(5,85);
-    cout << "\n Square Root Function : " << sqrt(64);
+    cout << "\nSquare Root Function : " << sqrt(64);
+    cout << "\nRound number function :" << round(4.4);
     return 0;
 }
