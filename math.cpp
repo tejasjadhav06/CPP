bb9de83e3a58a0b6a6980471cfbbe55d1597cec2
tejasjadhav06 <jadhavtejas405@gmail.com>
2024-09-25 16:30:53 +0530

    Minimum number from two number

--- math.cpp
+++ math.cpp
@@ -2,5 +2,6 @@
 using namespace std;
 int main(){
     cout << "Maximum number from two number is : " << max(5,85);
+    cout << "\nMinimum number from two number is : " << max(5,85);
     return 0;
 }
