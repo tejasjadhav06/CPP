564607f5b004c8b1dbd94d4ae3bc39d805aaa760
tejasjadhav06 <jadhavtejas405@gmail.com>
2024-09-29 23:18:13 +0530

    While Loop

--- loops.cpp
+++ loops.cpp
@@ -1,8 +1,17 @@
 #include<iostream>
 using namespace std;
 int main(){
+
     for(int i=1;i<11;i++){
         cout<< i << " ";
+    }
+    
+    int i=1;
+    while(i<=100){
+        if(i % 2 !=0){
+            cout << i << " ";
+        }
+        i++;
     }
 
 }
